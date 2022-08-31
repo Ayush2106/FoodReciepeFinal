23887ebf5655f4c9c0cfe31e4bcf332e079ada0e
Ayush Singla <84726355+Ayush2106@users.noreply.github.com>
2022-09-01 00:01:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Meal from './Components/Meal';
 import Recipe from './Components/Recipe';
 import {Route,Routes} from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
+// import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <Route  path="/" element={<Meal/>}/>
       <Route exact path="/:recipeId" element={<Recipe/>}/>
       </Routes>
-      <Footer/>
+//       <Footer/>
     </>
   )
 }
