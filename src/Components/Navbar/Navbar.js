b21d35b05394296aa8d8ffbc09401d88c659a4e4
Ayush Singla <84726355+Ayush2106@users.noreply.github.com>
2022-09-01 08:06:52 +0530

    Update Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -13,41 +13,7 @@ const Navbar = () => {
         
           </div>
       </div>
-      {/* right */}
-      <div className="n-right">
-        <div className="n-list">
-          <ul style={{ listStyleType: "none" }}>
-            <li>
-              {/* <Link activeClass="active" to="Navbar" spy={true} smooth={true}> */}
-                Home
-              {/* </Link> */}
-            </li>
-            <li>
-              {/* <Link to="about" spy={true} smooth={true}> */}
-                About
-              {/* </Link> */}
-            </li>
-            <li>
-              {/* <Link to="portfolio" spy={true} smooth={true}> */}
-                Portfolio
-              {/* </Link> */}
-            </li>
-            <li>
-              {/* <Link to="r" spy={true} smooth={true}> */}
-                Skills
-              {/* </Link> */}
-            </li>
-            {/* <li>
-              <Link to="footer" spy={true} smooth={true}>
-                Contact
-              </Link>
-            </li> */}
-          </ul>
-        </div>
-        {/* <Link to="footer" spy={true} smooth={true}> */}
-        <button className="button n-button">Contact</button>
-        {/* </Link> */}
-      </div>
+      
     </div>
   );
 };
